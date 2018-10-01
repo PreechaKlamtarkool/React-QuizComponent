8ddb292d403d1e7b11fc2caf2b88b3d19d0d819a
Boy <preecha.klam@gmail.com>
2018-10-01 10:07:52 +0700

    finis part 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,12 +11,11 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_question.length)
+        const isQuizEnd = true
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
-                }
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>}
             </div>
         )
     }
--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 
 class QuizEnd extends Component{
     render(){
-        retrun(
+        return(
             <div>
-                <p>Thank for playing!</p>
-                <a href='#'>Reset Quiz</a>
+                <p>Thanks for playing!</p>
+                <a href=''>Reset Quiz</a>
             </div>
         )
     }
-
 }
 
 export default QuizEnd
