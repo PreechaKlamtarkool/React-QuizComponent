e7d8adb5ef73b8c037acb9bf197d392e966e8b81
Boy <preecha.klam@gmail.com>
2018-10-01 09:57:49 +0700

    finis part 4

---
+++ src/QuizEnd.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class QuizEnd extends Component{
+    render(){
+        retrun(
+            <div>
+                <p>Thank for playing</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        )
+    }
+
+}
+
+export default QuizEnd
